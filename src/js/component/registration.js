f91db047897e3567a67aca42401deca832c1c4e8
Kevin Castillo <kevincastillo318@gmail.com>
2020-03-07 01:29:23 +0000

    finished onChange

--- src/js/component/registration.js
+++ src/js/component/registration.js
@@ -42,7 +42,12 @@ export const Registration = () => {
 					</Form.Group>
 					<Form.Group as={Col} md="4" controlId="validationCustom02">
 						<Form.Label>Last name</Form.Label>
-						<Form.Control required type="text" placeholder="Last name" onChange={e => setLastname(e.target.value)}/>
+						<Form.Control
+							required
+							type="text"
+							placeholder="Last name"
+							onChange={e => setLastname(e.target.value)}
+						/>
 						<Form.Control.Feedback>Looks good!</Form.Control.Feedback>
 					</Form.Group>
 					<Form.Group as={Col} md="4" controlId="validationEmail">
@@ -52,8 +57,8 @@ export const Registration = () => {
 								type="text"
 								placeholder="E-mail"
 								aria-describedby="inputGroupPrepend"
-                                required
-                                onChange={e => setEmail(e.target.value)}
+								required
+								onChange={e => setEmail(e.target.value)}
 							/>
 							<Form.Control.Feedback type="invalid">Please insert valid e-mail.</Form.Control.Feedback>
 						</InputGroup>
@@ -62,17 +67,27 @@ export const Registration = () => {
 				<Form.Row>
 					<Form.Group as={Col} md="6" controlId="validationCustom03">
 						<Form.Label>City</Form.Label>
-						<Form.Control type="text" placeholder="City" required onChange={e => setCity(e.target.value)}/>
+						<Form.Control type="text" placeholder="City" required onChange={e => setCity(e.target.value)} />
 						<Form.Control.Feedback type="invalid">Please provide a valid city.</Form.Control.Feedback>
 					</Form.Group>
 					<Form.Group as={Col} md="3" controlId="validationCustom04">
 						<Form.Label>State</Form.Label>
-						<Form.Control type="text" placeholder="State" required onChange={e => setState(e.target.value)}/>
+						<Form.Control
+							type="text"
+							placeholder="State"
+							required
+							onChange={e => setState(e.target.value)}
+						/>
 						<Form.Control.Feedback type="invalid">Please provide a valid state.</Form.Control.Feedback>
 					</Form.Group>
 					<Form.Group as={Col} md="3" controlId="validationCustom05">
 						<Form.Label>Zip</Form.Label>
-						<Form.Control type="text" placeholder="Zip" required onChange={e => setZipcode(e.target.value)}/>
+						<Form.Control
+							type="text"
+							placeholder="Zip"
+							required
+							onChange={e => setZipcode(e.target.value)}
+						/>
 						<Form.Control.Feedback type="invalid">Please provide a valid zip.</Form.Control.Feedback>
 					</Form.Group>
 				</Form.Row>
@@ -84,9 +99,9 @@ export const Registration = () => {
 					/>
 				</Form.Group>
 				<Button
-					type="submit"
+					type="button"
 					onClick={() => {
-						console.log({ firstname }, { lastname}, {state});
+						console.log({ firstname }, { lastname }, { state });
 					}}>
 					Submit
 				</Button>
