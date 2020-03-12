497441252c42d040054069d709d590d6e2c90c99
Kevin Castillo <kevincastillo318@gmail.com>
2020-03-12 01:21:27 +0000

    register nav link fixed

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -24,7 +24,7 @@ export const NavbarNew = () => {
 							Log In
 						</Nav.Link>
 						<Nav.Link className="nav-link text-white" href="#">
-							<Link className="link registration-link nav-link text-white" to="/registration">
+							<Link className="link registration-link nav-link" to="/registration">
 								Register
 							</Link>
 						</Nav.Link>
@@ -47,60 +47,3 @@ export const NavbarNew = () => {
 		</Navbar>
 	);
 };
-
-{
-	/* <nav className="navbar navbar-expand-lg navbar-light font-weight-bold pt-3">
-		// 	<Link className="link registration-link nav-link text-white" to="/">
-		// 		Liberty Mall
-		// 	</Link>
-		// 	<button */
-}
-// 		className="navbar-toggler"
-// 		type="button"
-// 		data-toggle="collapse"
-// 		data-target="#navbarNavDropdown"
-// 		aria-controls="navbarNavDropdown"
-// 		aria-expanded="false"
-// 		aria-label="Toggle navigation">
-// 		<span className="navbar-toggler-icon"></span>
-// 	</button>
-// 	<div className="collapse navbar-collapse text-white" id="navbarNavDropdown">
-// 		<div className="rightside ml-auto">
-// 			<ul className="navbar-nav text-white">
-// 				<li className="nav-item active">
-// 					<a className="nav-link text-white" href="#" onClick={e => setModal(true)}>
-// 						Log In
-// 					</a>
-// 				</li>
-// 				<li className="nav-item active">
-// 					<Link className="link registration-link nav-link text-white" to="/registration">
-// 						Register
-// 					</Link>
-// 				</li>
-// 				<li className="nav-item active">
-// 					<a className="nav-link text-white" href="#">
-// 						FAQ
-// 					</a>
-// 				</li>
-// 				<li className="nav-item active">
-// 					<a className="nav-link text-white" href="#">
-// 						Contacts
-// 					</a>
-// 				</li>
-// 				<li className="nav-item active">
-// 					<a className="nav-link text-white" href="#">
-// 						About Us
-// 					</a>
-// 				</li>
-// 				<li className="nav-item active">
-// 					<a className="nav-link text-white" href="#">
-// 						<i className="fas fa-shopping-cart fa-lg"></i>
-// 					</a>
-// 				</li>
-// 			</ul>
-// 		</div>
-// 		<LogInModal show={modal} onHide={e => setModal(false)} />
-// 	</div>
-// </nav>
-// 	);
-// };
