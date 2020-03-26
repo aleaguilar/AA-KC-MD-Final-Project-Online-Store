c01a21048d07a047328332de11ae2307f292c1dd
Marko Davidovic <davidovic-m@hotmail.com>
2020-03-26 17:31:31 +0000

    shopping cart view

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -8,8 +8,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			message: "",
 			database: [
 				{
-					title:
-						"TOPESEL 32GB Micro SD Card 2 Pack Memory Cards Micro SDHC UHS-I TF Card Class 10 for Cemera/Drone/Dash Cam(2 Pack U1 32GB)",
+					title: "TOPESEL 32GB Micro SD Card 2 Pack Memory Cards with High Speed Compatible",
 					asin: "B07Z7V34RG",
 					link:
 						"https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=sspa_dk_detail_8?ie=UTF8&adId=A0194495351Y18X7XSCFH&qualifier=1584522116&id=7310208867779930&widgetName=sp_detail2&url=%2Fdp%2FB07Z7V34RG%2Fref%3Dsspa_dk_detail_8%3Fpsc%3D1",
@@ -25,8 +24,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}
 				},
 				{
-					title:
-						"2 Pack of 128GB MicroSD Card with Adapter,U3 A1 MicroSDXC Card 667X High Speed Up to 100MB/s UHS-I Micro SD 128 GB UHS-1 Memory Card for Android Smartphone Nintendo Galaxy Fire and Gopro",
+					title: "2 Pack of 128GB MicroSD Card with Adapter,U3 A1 MicroSDXC Card 667X",
 					asin: "B07YG4TMYL",
 					link:
 						"https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=sspa_dk_detail_6?ie=UTF8&adId=A097389530Q9W9NMDT7JM&qualifier=1584522116&id=7310208867779930&widgetName=sp_detail2&url=%2Fdp%2FB07YG4TMYL%2Fref%3Dsspa_dk_detail_6%3Fpsc%3D1",
@@ -36,14 +34,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 					is_prime: true,
 					price: {
 						symbol: "$",
-						value: 42.98,
+						value: 42.99,
 						currency: "USD",
-						raw: "$42.98"
+						raw: "$42.99"
 					}
 				},
 				{
-					title:
-						"Estone 5pcs 1GB SD Cards Security Digital Memory Card with High Speed Compatible with Cameras Camcorders Computers Card Readers and Other SD Card Compatible Devices",
+					title: "Estone 5pcs 1GB SD Cards Security Digital Memory Card with High Speed Compatible",
 					asin: "B00N3RMW3A",
 					link:
 						"https://www.amazon.com/gp/slredirect/picassoRedirect.html/ref=sspa_dk_detail_7?ie=UTF8&adId=A02363681UC9JYCAOGFUO&qualifier=1584522116&id=7310208867779930&widgetName=sp_detail2&url=%2Fdp%2FB00N3RMW3A%2Fref%3Dsspa_dk_detail_7%3Fpsc%3D1",
@@ -62,7 +59,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 			create_user: "",
 			token: null,
 			cart: [],
-			cartTotal: 0
+			cartTotal: 0,
+			removeFromCart: []
 		},
 		actions: {
 			createContact: email => {
@@ -168,6 +166,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 				// console.log(cartTotal);
 
 				store.cartTotal = cartTotal.reduce((total, current) => total + current, 0);
+
+				setStore(store);
+			},
+			removeFromCart: () => {
+				let store = getStore();
+
+				let removeFromCart = store.removeFromCart.slice(item => {
+					return item.count - item.price;
+				});
+
+				store.removeFromCart = removeFromCart.slice((total, current) => total - current, 0);
+				console.log(removeFromCart);
 
 				setStore(store);
 			}
