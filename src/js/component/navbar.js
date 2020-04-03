4e768304a9c6b6e2786fecc57d72b23149bc4dd0
Marko Davidovic <davidovic-m@hotmail.com>
2020-04-03 18:17:12 -0400

    Logo added

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -20,7 +20,7 @@ export const NavbarNew = () => {
 		<Navbar expand="lg" className=" sticky navbar navbar-light font-weight-bold pt-3">
 			<Navbar.Brand href="#home">
 				<Link className="link registration-link nav-link text-white" to="/">
-					{/* <img src="/logo.png" /> */}Liberty Mall
+					<img src="/logo.png" />
 				</Link>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
